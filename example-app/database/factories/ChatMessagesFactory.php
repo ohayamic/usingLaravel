ab3b07d2839a7777627cce50ce60c267c57e0947
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2021-03-07 08:59:03 +0100

    Added ChatMessageFactory

---
+++ example-app/database/factories/ChatMessagesFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ChatMessages;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ChatMessagesFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ChatMessages::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        
+        return [
+            //
+            'messages' => $this->faker->text,
+            'user_id' =>\App\Models\User::query()
+                            ->inRandomOrder()
+                            ->firstOr(function () {
+                                return factory(\App\Models\User::class)->create();
+                        }),
+            'chat_room_id' =>\App\Models\ChatRoom::query()
+                            ->inRandomOrder()
+                            ->firstOr(function () {
+                                 return factory(\App\Models\ChatRoom::class)->create();
+                            }),
+
+        ];
+    }
+}
