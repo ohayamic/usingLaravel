dbfc8c20d68344f5ab23742ce745efccb98d04c6
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2021-03-07 08:59:25 +0100

    Added the ChatRoomFactory

---
+++ example-app/database/factories/ChatRoomFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ChatRoom;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ChatRoomFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ChatRoom::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+            'name' => $this->faker->sentence($nbWords = 6, $variableNbWords = true),
+            'user_id' =>\App\Models\User::query()
+                    ->inRandomOrder()
+                    ->firstOr(function () {
+                    return factory(\App\Models\User::class)->create();
+        }),
+        ];
+    }
+}
