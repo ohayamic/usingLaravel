6ec0896a2dc5b0245ebdc5b0eef7496f05266911
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2021-03-07 09:01:33 +0100

    updated the databaseseeder to include chatRoom and chatMessage seeds

--- example-app/database/seeders/DatabaseSeeder.php
+++ example-app/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\ChatRoom::factory(10)->create();
+        \App\Models\ChatMessages::factory(10)->create();
     }
 }
