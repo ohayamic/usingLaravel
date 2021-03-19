56d66c63daa1c1f56092f30291d0d5c539a93da7
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2021-03-19 18:35:55 +0100

    Updated dataBaseSeeder

--- example-app/database/seeders/DatabaseSeeder.php
+++ example-app/database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-        \App\Models\ChatRoom::factory(10)->create();
-        \App\Models\ChatMessages::factory(10)->create();
+        \App\Models\User::factory(100)->create();
+        \App\Models\ChatRoom::factory(100)->create();
+        \App\Models\ChatMessages::factory(100)->create();
     }
 }
